17b412494c57c9b4d51b41d76d85938a1bcf0c56
Iva Dimeska <ivadimeska998@gmail.com>
2023-08-29 09:43:00 +0200

    Added one more test for the task

---
+++ src/test/java/org/example/AddBookTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class AddBookTest {
+    ChromeDriver chromeDriver = new ChromeDriver();
+    @Test
+    public void testName(){
+        chromeDriver.findElement(By.xpath("//a[@href='/books?book=9781449325862']")).click();
+        chromeDriver.findElement(By.id("addNewRecordButton")).click();
+    }
+
+    @BeforeTest
+    public void logIn()
+    {
+        chromeDriver.get("https://demoqa.com/books");
+        chromeDriver.manage().window().maximize();
+        chromeDriver.findElement(By.id("login")).click();
+        chromeDriver.findElement(By.id("userName")).sendKeys("idm");
+        chromeDriver.findElement(By.id("password")).sendKeys("Jassumiva123!");
+        chromeDriver.findElement(By.id("login")).click();
+    }
+}
